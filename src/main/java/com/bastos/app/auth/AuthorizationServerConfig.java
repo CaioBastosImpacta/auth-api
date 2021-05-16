05f2e91d63c592868006c192bc4dcc0ba6d8176f
CaioBastosImpacta <caio.bastos@aluno.faculdadeimpacta.com.br>
2021-05-15 23:09:50 -0300

    criando o repo e api de auth

---
+++ src/main/java/com/bastos/app/auth/AuthorizationServerConfig.java
@@ -0,0 +1,53 @@
+package com.bastos.app.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+            .inMemory()
+                .withClient("bastos-auth-web")
+                .secret(passwordEncoder.encode("bastos123456"))
+                .authorizedGrantTypes("password", "refresh_token")
+                .scopes("write", "read")
+            .accessTokenValiditySeconds(60 * 60 * 6) // 6 horas (padrão é 12 horas)
+        .and()
+                .withClient("check-bastos-crud")
+                .secret(passwordEncoder.encode("checkbastos123456"));
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+//		security.checkTokenAccess("isAuthenticated()");
+        security.checkTokenAccess("permitAll()");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints
+            .authenticationManager(authenticationManager)
+            .userDetailsService(userDetailsService);
+    }
+}
