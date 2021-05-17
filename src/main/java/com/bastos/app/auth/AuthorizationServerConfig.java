7ffde90b55caf6aea4f7172c21e9dc69a67450cf
CaioBastosImpacta <caio.bastos@aluno.faculdadeimpacta.com.br>
2021-05-16 22:23:43 -0300

    autenticacao jwt

--- src/main/java/com/bastos/app/auth/AuthorizationServerConfig.java
+++ src/main/java/com/bastos/app/auth/AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package com.bastos.app.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,6 +11,11 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.CompositeTokenGranter;
+import org.springframework.security.oauth2.provider.TokenGranter;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableAuthorizationServer
@@ -36,6 +42,19 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
             .refreshTokenValiditySeconds(60 * 24 * 60 * 60) // 60 dias (padrão é 30 horas)
 
             .and()
+                .withClient("foodanalytics")
+                .secret(passwordEncoder.encode(""))
+                .authorizedGrantTypes("authorization_code")
+                .scopes("write", "read")
+                .redirectUris("http://www.foodanalytics.local:8082")
+
+            .and()
+                .withClient("webadmin")
+                .authorizedGrantTypes("implicit")
+                .scopes("write", "read")
+                .redirectUris("http://aplicacao-cliente")
+
+            .and()
                 .withClient("bastos-auth-crud")
                 .secret(passwordEncoder.encode("bastosCrud123456"))
                 .authorizedGrantTypes("client_credentials")
@@ -49,14 +68,36 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 //		security.checkTokenAccess("isAuthenticated()");
-        security.checkTokenAccess("permitAll()");
+        security.checkTokenAccess("permitAll()")
+                .allowFormAuthenticationForClients();
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints
             .authenticationManager(authenticationManager)
-            .userDetailsService(userDetailsService);
-//            .reuseRefreshTokens(false);
+            .userDetailsService(userDetailsService)
+			.reuseRefreshTokens(false)
+            .accessTokenConverter(jwtAccessTokenConverter())
+            .tokenGranter(tokenGranter(endpoints));
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        jwtAccessTokenConverter.setSigningKey("fi156ygty345qsnmjçihbeinv30j320fw3ndmpfg34g43gn3g0ivnvn0g2f0f30");
+
+        return jwtAccessTokenConverter;
+    }
+
+    private TokenGranter tokenGranter(AuthorizationServerEndpointsConfigurer endpoints) {
+        var pkceAuthorizationCodeTokenGranter = new PkceAuthorizationCodeTokenGranter(endpoints.getTokenServices(),
+                endpoints.getAuthorizationCodeServices(), endpoints.getClientDetailsService(),
+                endpoints.getOAuth2RequestFactory());
+
+        var granters = Arrays.asList(
+                pkceAuthorizationCodeTokenGranter, endpoints.getTokenGranter());
+
+        return new CompositeTokenGranter(granters);
     }
 }
